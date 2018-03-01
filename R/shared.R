72e6b9ae4aace097f0a31f2b8d3507e446dec4d3
Kimyen <kimyen0816@gmail.com>
2018-03-01 14:36:42 -0800

    omit some functions

--- R/shared.R
+++ R/shared.R
@@ -2,7 +2,15 @@
   pyImport("synapseutils")
   pyExec("info = functionInfo.functionInfo(synapseutils)")
   functionInfo <- pyGet("info")
+  toOmit <- c("with_progress_bar", "notifyMe")
   result <- lapply(X = functionInfo, function(x) {
+    if (any(x$name==toOmit)) return(NULL)
     list(name = x$name, synName = x$name, functionContainerName = "synapseutils", args = x$args, doc = x$doc, title = x$name)
   })
+  # scrub the nulls
+  nullIndices<-sapply(result, is.null)
+  if (any(nullIndices)) {
+    result<-result[-which(nullIndices)]
+  }
+  result
 }
