d53cde1d8209316c95d62bbe1fe0a1b448bffb37
thomasyu888 <thomas.yu@sagebase.org>
2023-01-23 22:28:55 -0800

    Add all missing files

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  # reticulate::py_run_string("import synapseclient")
+  reticulate::py_run_string("import synapseclient")
   # reticulate::py_run_string(sprintf("synapserVersion = 'synapser/%s' ", utils::packageVersion("synapser")))
   # reticulate::py_run_string("synapseclient.USER_AGENT['User-Agent'] = synapserVersion + synapseclient.USER_AGENT['User-Agent']")
   # reticulate::py_run_string("synapseclient.core.config.single_threaded = True")
-  # reticulate::py_run_string("syn=synapseclient.Synapse(skip_checks=True)")
+  reticulate::py_run_string("syn=synapseclient.Synapse(skip_checks=True)")
   # # make syn available in the global environment
-  # syn <<- reticulate::py_eval("syn")
+  syn <<- reticulate::py_eval("syn")
   .addPythonWrapperToSearchPath(system.file(package = "synapserutils"))
   .defineUtilFunctions()
   reticulate::import("synapseUtilsWrapper")
@@ -17,9 +17,9 @@
 }
 
 .addPythonWrapperToSearchPath <- function(srcDir) {
-  reticulate::py_run_string("sys")
+  reticulate::py_run_string("import sys")
   reticulate::py_run_string(sprintf("sys.path.append('%s')", file.path(srcDir, "python")))
-  reticulate::py_run_string("synapseUtilsWrapper")
+  reticulate::import("synapseUtilsWrapper")
 }
 
 .defineUtilFunctions <- function() {
